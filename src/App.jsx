fc689eed463a02a8bbb948a050598166886879f8
Willian M. Vargas <willianm.vargas@gmail.com>
2023-01-11 16:50:53 -0300

    requisito-01-group-6

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    </div>
+  );
+}
